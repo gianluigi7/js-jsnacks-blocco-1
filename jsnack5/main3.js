2091b1a2ed6073d5038c27e523c99fc352c2db0f
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-06 01:34:40 +0200

    snack 5

---
+++ jsnack5/main3.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let numeriDispari = [];
+
+
+for (let i = 0; i < 6; i++) {
+ 
+    numeriDispari[i]=Number(prompt (`inserire un numero ${i}`));
+
+    if (numeriDispari.value % 2 === false) {
+
+       
+    }
+}
+
+console.log(numeriDispari);
---
+++ jsnack5/readme.md
@@ -0,0 +1,5 @@
+creare un array vuoto
+
+ciclo for con il valore di un prompt numerico da ripetere 6 volte 
+
+de dispari inserire nell'array 
